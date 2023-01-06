d0d9774b6cbbbc5805811344a886e33757c68e92
kelseyn12 <kelseynocek12@gmail.com>
2023-01-06 16:07:26 -0600

    added content to footer

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,15 @@
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 import { AllPrograms } from '../src/components/home/all-programs'
-import { Header } from '../src/components/header/header'
-import { Footer } from '../src/components/footer/footer'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({data}) {
   return (
     <>
-      
-      <Header />
-      
+       
       <AllPrograms data={data} />
-      <Footer />
+
     </>
   )
 }
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,36 @@
 export const Footer = () => (
     <footer>
-        FOOTER
+        <br />
+        <div>
+        <div>
+            VILLAGE
+        </div>
+        <div>© 2010-2020</div>
+        <div>Privacy - Terms</div>
+        <div>
+            Find Programs 
+            Product 
+            Product 
+            Product 
+        </div>
+        <div>
+            Plan Programs 
+            Feature 
+            Feature 
+            Feature 
+        </div>
+        <div>
+            Resources 
+            Resource 
+            Resource 
+            Resource 
+        </div>
+        <div>
+            About Us 
+            Company 
+            FAQs 
+            How it Works 
+        </div>
+        </div>
     </footer>
 )
