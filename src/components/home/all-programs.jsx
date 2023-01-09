1a1ce4e481af33f6bae7901053b75779e23d0cda
kelseyn12 <kelseynocek12@gmail.com>
2023-01-09 16:09:06 -0600

    wip: installed MUI and added the pagination component to all programs

--- src/components/home/all-programs.jsx
+++ src/components/home/all-programs.jsx
@@ -1,29 +1,44 @@
+import { Pagination } from "@material-ui/lab";
+import { useState } from "react";
+
 const options = {
-  year: 'numeric',
-  month: 'short',
-  day: 'numeric',
-  weekday: 'short'
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+  weekday: "short",
 };
 
 export const AllPrograms = ({ data }) => {
-  const formattedData = data.map(i => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+  const numPages = Math.ceil(data.length / itemsPerPage);
+
+  const formattedData = data.map((i) => {
     const startDate = new Date(i.date_begin);
     const endDate = new Date(i.date_end);
-    const formattedStartDate = startDate.toLocaleDateString('en-US', options);
-    const formattedEndDate = endDate.toLocaleDateString('en-US', options);
+    const formattedStartDate = startDate.toLocaleDateString("en-US", options);
+    const formattedEndDate = endDate.toLocaleDateString("en-US", options);
 
     return {
       name: i.name,
       grade: i.grade,
       date_begin: formattedStartDate,
       date_end: formattedEndDate,
-      location: i.location
+      location: i.location,
     };
   });
 
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentPageData = formattedData.slice(startIndex, endIndex);
+
   return (
     <main>
-      {formattedData.map((evt) => (
+      {currentPageData.map((evt) => (
         <div key={evt.name}>
           <h1>{evt.name}</h1>
           <p>Grades {evt.grade}</p>
@@ -35,7 +50,11 @@ export const AllPrograms = ({ data }) => {
           <a href="/">Learn More</a>
         </div>
       ))}
+      <Pagination
+        count={numPages}
+        page={currentPage}
+        onChange={handlePageChange}
+      />
     </main>
   );
 };
-
